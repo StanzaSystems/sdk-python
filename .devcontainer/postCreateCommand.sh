b7c4d35564d72373e5e44a66ccf521c54d009afc
Matthew Girard <msg@sre.is>
2023-11-01 22:33:01 +0000

    add poetry to system python

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,9 @@ DEBIAN_FRONTEND=noninteractive \
     sudo apt-get -y install --no-install-recommends \
     apt-utils dialog dnsutils
 
+sudo pip3 install --upgrade pip
+sudo pip3 install poetry
+
 # Because I'm tired of remembering to do this everytime I rebuild my devcontainer
 echo '-w "\\n"' > ~/.curlrc
 echo 'alias ll="ls -l"' >> ~/.bashrc
