d9b179fe2f34babddd2b2d73c7502b2db6128ab5
Matthew Girard <msg@sre.is>
2023-11-01 21:53:50 +0000

    nit: readability

--- packages/getstanza/__init__.py
+++ packages/getstanza/__init__.py
@@ -17,34 +17,34 @@ def init(
     """Use to initialize Stanza SDK"""
     logging.debug("Initializing Stanza")
 
-    if api_key is None or api_key == "":
-        if os.environ.get("STANZA_API_KEY", "") != "":
+    if not api_key:
+        if os.environ.get("STANZA_API_KEY"):
             api_key = os.environ.get("STANZA_API_KEY")
         else:
             raise ValueError(
                 "Missing required Stanza API key (Hint: Set a STANZA_API_KEY environment variable!)"
             )
 
-    if service_name is None or service_name == "":
-        if os.environ.get("STANZA_SERVICE_NAME", "") != "":
+    if not service_name:
+        if os.environ.get("STANZA_SERVICE_NAME"):
             service_name = os.environ.get("STANZA_SERVICE_NAME")
         else:
             service_name = "unknown_service"
 
-    if service_release is None or service_release == "":
-        if os.environ.get("STANZA_SERVICE_RELEASE", "") != "":
+    if not service_release:
+        if os.environ.get("STANZA_SERVICE_RELEASE"):
             service_release = os.environ.get("STANZA_SERVICE_RELEASE")
         else:
             service_release = "0.0.0"
 
-    if service_environment is None or service_environment == "":
-        if os.environ.get("STANZA_ENVIRONMENT", "") != "":
+    if not service_environment:
+        if os.environ.get("STANZA_ENVIRONMENT"):
             service_release = os.environ.get("STANZA_ENVIRONMENT")
         else:
             service_release = "unknown"
 
-    if hub_address is None or hub_address == "":
-        if os.environ.get("STANZA_HUB_ADDRESS", "") != "":
+    if not hub_address:
+        if os.environ.get("STANZA_HUB_ADDRESS"):
             hub_address = os.environ.get("STANZA_HUB_ADDRESS")
         else:
             hub_address = "https://hub.stanzasys.co"
